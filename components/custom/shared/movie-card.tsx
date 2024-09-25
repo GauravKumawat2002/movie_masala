2f69c959b098f09a50543a573cf35a2c1d28e7b3
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-25 22:48:54 +0530

    Changes: Implements fuctionality that it renders different UI based on the cardType Prop pass to it

--- components/custom/shared/movie-card.tsx
+++ components/custom/shared/movie-card.tsx
@@ -1,39 +1,66 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { useSelectedMovieStore } from "@/store/global-store";
 import Image from "next/image";
 export default function MovieCard({
   movie,
-  handleSelected,
+  cardType,
 }: {
-  movie: Movie;
-  handleSelected: (id: string) => void;
+  movie: Movie | WatchedMovie;
+  cardType: "MovieCard" | "WatchedMovieCard";
 }) {
-  return (
-    <li onClick={() => handleSelected(movie.imdbID)}>
-      <Card className="justify-between">
-        <CardHeader>
-          <Image
-            src={movie.Poster}
-            alt={`${movie.Title} poster`}
-            width={150}
-            height={200}
-            className="aspect-auto h-[200px]"
-          />
-          <CardTitle className="text-balance">{movie.Title} </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <CardDescription>
-            <span>🗓</span>
-            <span>{movie.Year}</span>
-          </CardDescription>
-        </CardContent>
-      </Card>
-    </li>
-  );
+  const setSelectedMovieId = useSelectedMovieStore(state => state.setSelectedMovieId);
+
+  if (cardType === "MovieCard")
+    return (
+      <li onClick={() => setSelectedMovieId(movie.imdbID)}>
+        <Card className="shadow-primary shadow-sm dark:shadow-background dark:shadow-lg">
+          <CardHeader>
+            <Image
+              src={movie.Poster === "N/A" ? "/" : movie.Poster}
+              alt={`${movie.Title} poster`}
+              width={150}
+              height={200}
+              className="w-full"
+            />
+            <CardTitle className="text-wrap">{movie.Title} </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <CardDescription>
+              <span>🗓</span> <span>{movie.Year}</span>
+            </CardDescription>
+          </CardContent>
+        </Card>
+      </li>
+    );
+
+  if (cardType === "WatchedMovieCard") {
+    const watchedMovie = movie as WatchedMovie;
+    return (
+      <li>
+        <Card className="shadow-primary shadow-sm dark:shadow-background dark:shadow-lg">
+          <CardHeader>
+            <Image
+              src={watchedMovie.Poster === "N/A" ? "/" : watchedMovie.Poster}
+              alt={`${watchedMovie.Title} poster`}
+              width={150}
+              height={200}
+              className="w-full"
+            />
+            <CardTitle className="text-wrap">{watchedMovie.Title} </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <CardDescription>
+              <span>🗓</span> <span>{watchedMovie.Year}</span>
+            </CardDescription>
+            <CardDescription>
+              <span>🗓</span> <span>{watchedMovie.imdbRating}</span>
+            </CardDescription>
+            <CardDescription>
+              <span>🗓</span> <span>{watchedMovie.runtime}</span>
+            </CardDescription>
+          </CardContent>
+        </Card>
+      </li>
+    );
+  }
 }
