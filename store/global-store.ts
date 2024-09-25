47bb9c95b83fd9457f41a81c3be8b3aa25a772ab
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-25 22:40:36 +0530

    Feat: global-store.ts uses zustand for global state management

---
+++ store/global-store.ts
@@ -0,0 +1,87 @@
+import { User } from "lucide-react";
+import { create } from "zustand";
+// movies
+type MoviesState = {
+  movies: Movie[];
+  setMovies: (movies: Movie[]) => void;
+};
+
+export const useMovieStore = create<MoviesState>(set => ({
+  movies: [],
+  setMovies: (movies: Movie[]) => set({ movies: movies }),
+}));
+
+// query
+type QueryState = {
+  query: string;
+  setQuery: (query: string) => void;
+};
+export const useQueryStore = create<QueryState>(set => ({
+  query: "",
+  setQuery: (query: string) => set({ query }),
+}));
+
+// loading
+type LoadingState = {
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
+};
+
+export const useLoadingStore = create<LoadingState>(set => ({
+  loading: false,
+  setLoading: (loading: boolean) => set({ loading }),
+}));
+
+// error
+type ErrorState = {
+  error: string;
+  setError: (error: string) => void;
+};
+
+export const useErrorStore = create<ErrorState>(set => ({
+  error: "",
+  setError: (error: string) => set({ error }),
+}));
+
+// movie selection
+type SelectedMovieState = {
+  selectedMovieId: string | null;
+  setSelectedMovieId: (id: string) => void;
+  resetSelectedMovieId: () => void;
+};
+export const useSelectedMovieStore = create<SelectedMovieState>(set => ({
+  selectedMovieId: null,
+  setSelectedMovieId: (id: string) => set({ selectedMovieId: id }),
+  resetSelectedMovieId: () => set({ selectedMovieId: null }),
+}));
+
+// userRating
+type UserRatingState = {
+  userRating: string;
+  setUserRating: (userRating: string) => void;
+};
+export const useUserRatingStore = create<UserRatingState>(set => ({
+  userRating: "",
+  setUserRating: (UserRating: string) => set({ userRating: UserRating }),
+}));
+
+// watchedMovies
+type WatchedMoviesState = {
+  watchedMovies: WatchedMovie[] | [];
+  addWatchedMovies: (watchedMovie: WatchedMovie) => void;
+  removeWatchedMovies: (id: string) => void;
+  resetWatchedMovies: () => void;
+};
+
+export const useWatchedMoviesStore = create<WatchedMoviesState>(set => ({
+  watchedMovies: [],
+  addWatchedMovies: (watchedMovie: WatchedMovie) =>
+    set(state => ({
+      watchedMovies: state.watchedMovies.some(movie => watchedMovie.imdbID === movie.imdbID)
+        ? state.watchedMovies
+        : [...state.watchedMovies, watchedMovie],
+    })),
+  removeWatchedMovies: (id: string) =>
+    set(state => ({ watchedMovies: state.watchedMovies.filter(movie => movie.imdbID !== id) })),
+  resetWatchedMovies: () => set({ watchedMovies: [] }),
+}));
