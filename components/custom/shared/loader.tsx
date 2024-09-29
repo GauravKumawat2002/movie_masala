ee0248eabd1fd1147ab009658f34eb8a6cf28b34
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-29 18:29:14 +0530

    Adds margin to the top of loader

--- components/custom/shared/loader.tsx
+++ components/custom/shared/loader.tsx
@@ -1,7 +1,7 @@
 import { Loader } from "lucide-react";
 export default function SpinningLoader() {
   return (
-    <div className="w-36 h-36 mx-auto ">
+    <div className="w-36 h-36 mx-auto mt-20">
       <Loader className="animate-spin w-full" />
     </div>
   );
