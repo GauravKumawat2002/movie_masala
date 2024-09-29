39c4641d679507979982c7db62d1de2482dda7bb
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-29 18:46:11 +0530

    Improved UI of the entire app

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,25 @@
 "use client";
-import { useMovieStore, useQueryStore } from "@/store/global-store";
-import { ModeToggle } from "@/components/custom/navbar/mode-toggle";
-import Navbar from "@/components/custom/navbar/nav-bar";
-import NumResult from "@/components/custom/navbar/num-results";
-import Search from "@/components/custom/navbar/search";
-import Container from "@/components/custom/shared/container";
 import ListBox from "@/components/custom/listbox/list-box";
-
+import WatchedBox from "@/components/custom/watchbox/watched-box";
 import MovieDetailsCard from "@/components/custom/shared/movie-detail-card";
+import { useOnWatchedMoviesStore, useSelectedMovieStore } from "@/store/global-store";
 
 export default function Home() {
-  const query = useQueryStore(state => state.query);
-  const movies = useMovieStore(state => state.movies);
-
+  const onWatchedBox = useOnWatchedMoviesStore(state => state.onWatchedMovies);
+  const selectedMovieId = useSelectedMovieStore(state => state.selectedMovieId);
   return (
     <>
-      <Navbar className="mt-4 gap-4 md:gap-0">
-        <Search />
-        {movies && <NumResult movies={movies} />}
-        <ModeToggle />
-      </Navbar>
-      <Container>
-        <ListBox className="mt-4 pr-4 w-1/2" query={query} />
-        <MovieDetailsCard className="mt-4 pl-4 w-1/2" />
-      </Container>
+      {selectedMovieId === null ? (
+        <>
+          {!onWatchedBox ? (
+            <ListBox className="mt-4 mb-16" />
+          ) : (
+            <WatchedBox className="mt-4 mb-16" />
+          )}
+        </>
+      ) : (
+        <MovieDetailsCard className="mt-3 pl-4 mb-16" />
+      )}
     </>
   );
 }
