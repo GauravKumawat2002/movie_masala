73e8476d8f97173e685d45b9183cb117377d71cd
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-29 18:24:26 +0530

    Changes: Adds a feature that automatically closes WatchedBox component as soon as a user searches a query

--- components/custom/navbar/search.tsx
+++ components/custom/navbar/search.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
 import { Input } from "@/components/ui/input";
-import { useQueryStore } from "@/store/global-store";
+import { useOnWatchedMoviesStore, useQueryStore } from "@/store/global-store";
 export default function Search() {
   const query = useQueryStore(state => state.query);
   const setQuery = useQueryStore(state => state.setQuery);
   const [inputValue, setInputValue] = useState(query);
+  const setOnWatchedBox = useOnWatchedMoviesStore(state => state.setOnWatchedMovies);
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     e.key === "Enter" && setQuery(inputValue);
+    e.key === "Enter" && setOnWatchedBox(false);
   };
   const inputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
