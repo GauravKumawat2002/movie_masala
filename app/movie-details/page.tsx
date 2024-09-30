55f3dcf0afcaa32eaff142ce8134fce9addf51c0
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-30 22:38:55 +0530

    "deleted unnecessary file"

--- app/movie-details/page.tsx
+++
@@ -1,5 +0,0 @@
-import MovieDetailsCard from "@/components/custom/shared/movie-detail-card";
-
-export default function MovieDetailsPage() {
-  return <MovieDetailsCard className="mt-3 pl-4 mb-16" />;
-}
