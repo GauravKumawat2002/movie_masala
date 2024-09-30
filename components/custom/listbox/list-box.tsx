ae0c6ed404b9df96bdd46be66de84d250d6c7e85
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-30 22:28:59 +0530

    Changes: 1. Removes: useEffect for data fetching, unnecessary state variables 2. Now uses custom useFetchMovies hook powered by React query for data fetching

--- components/custom/listbox/list-box.tsx
+++ components/custom/listbox/list-box.tsx
@@ -1,75 +1,35 @@
 "use client";
-import { useEffect } from "react";
-import { useErrorStore, useLoadingStore, useMovieStore, useQueryStore } from "@/store/global-store";
+import KEY from "@/KEY";
+import { useMovieStore, useQueryStore } from "@/store/global-store";
 import MovieBox from "@/components/custom/shared/movie-box";
 import MovieCard from "@/components/custom/shared/movie-card";
 import SpinningLoader from "../shared/loader";
-import KEY from "@/KEY";
+import useFetchMovies from "@/hooks/use-fetchMovies";
 
 export default function ListBox({ className }: { className?: string }) {
-  const query = useQueryStore(state => state.query);
-  // state for movies
-  const movies = useMovieStore(state => state.movies);
-  const setMovies = useMovieStore(state => state.setMovies);
-  //  state for loading
-  const loading = useLoadingStore(state => state.loading);
-  const setLoading = useLoadingStore(state => state.setLoading);
-  // state for error
-  const error = useErrorStore(state => state.error);
-  const setError = useErrorStore(state => state.setError);
+  const query = useQueryStore((state) => state.query);
+  const setMovies = useMovieStore((state) => state.setMovies);
+  const { movies, isLoading, error, isSuccess } = useFetchMovies({
+    url: `https://www.omdbapi.com/?i=tt1375666&apikey=${KEY}&s=${query}`,
+  });
 
-  useEffect(
-    function () {
-      const controller = new AbortController();
-      async function fetchMovies() {
-        setLoading(true);
-
-        try {
-          const response = await fetch(
-            `https://www.omdbapi.com/?i=tt1375666&apikey=${KEY}&s=${query}`,
-            { signal: controller.signal, method: "GET" }
-          );
-          if (!response.ok) throw new Error("Failed to fetch Movies");
-          const data = await response.json();
-          if (data.Response === "False") throw new Error(data.Error);
-          setMovies(data.Search);
-        } catch (err) {
-          if (err instanceof Error && err.name !== "AbortError") {
-            setError(err instanceof Error ? err.message : "An unknown error occurred");
-          }
-        } finally {
-          setLoading(false);
-        }
-      }
-
-      if (!query.length) {
-        setError("");
-        setMovies([]);
-        return;
-      }
-
-      fetchMovies();
-      return () => {
-        setError("");
-        controller.abort();
-      };
-    },
-    [query, setMovies, setLoading, setError]
-  );
+  if (isSuccess && Array.isArray(movies)) setMovies(movies);
 
   return (
     <>
-      {loading && <SpinningLoader />}
-      {!loading && !error && movies && (
+      {isLoading && <SpinningLoader />}
+      {!isLoading && !error && Array.isArray(movies) && (
         <MovieBox className={className}>
-          {movies.map(movie => (
+          {movies.map((movie: Movie) => (
             <MovieCard cardType="MovieCard" key={movie.imdbID} movie={movie} />
           ))}
         </MovieBox>
       )}
-      {!loading && error && (
-        <div className="grid place-items-center mt-20">
-          <p className="text-semibold text-3xl text-red-800 dark:text-red-500"> {error}</p>
+      {!isLoading && error && (
+        <div className="mt-20 grid place-items-center">
+          <p className="text-semibold text-3xl text-red-800 dark:text-red-500">
+            {error.message}
+          </p>
         </div>
       )}
     </>
