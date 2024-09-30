93abc0065a818770d8cec2167e5fb99c4eb73906
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-09-30 22:25:30 +0530

    Remove: Unnecessary stores

--- store/global-store.ts
+++ store/global-store.ts
@@ -1,16 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-// movies
-type MoviesState = {
-  movies: Movie[];
-  setMovies: (movies: Movie[]) => void;
-};
-export const useMovieStore = create<MoviesState>((set) => ({
-  movies: [],
-  setMovies: (movies: Movie[]) => set({ movies: movies }),
-}));
-
 // query
 type QueryState = {
   query: string;
@@ -21,38 +11,6 @@ export const useQueryStore = create<QueryState>((set) => ({
   setQuery: (query: string) => set({ query }),
 }));
 
-// loading
-type LoadingState = {
-  loading: boolean;
-  setLoading: (loading: boolean) => void;
-};
-export const useLoadingStore = create<LoadingState>((set) => ({
-  loading: false,
-  setLoading: (loading: boolean) => set({ loading }),
-}));
-
-// error
-type ErrorState = {
-  error: string;
-  setError: (error: string) => void;
-};
-export const useErrorStore = create<ErrorState>((set) => ({
-  error: "",
-  setError: (error: string) => set({ error }),
-}));
-
-// movie selection
-type SelectedMovieState = {
-  selectedMovieId: string | null;
-  setSelectedMovieId: (id: string) => void;
-  resetSelectedMovieId: () => void;
-};
-export const useSelectedMovieStore = create<SelectedMovieState>((set) => ({
-  selectedMovieId: null,
-  setSelectedMovieId: (id: string) => set({ selectedMovieId: id }),
-  resetSelectedMovieId: () => set({ selectedMovieId: null }),
-}));
-
 // userRating
 type UserRatingState = {
   userRating: string;
@@ -61,6 +19,27 @@ type UserRatingState = {
 export const useUserRatingStore = create<UserRatingState>((set) => ({
   userRating: "",
   setUserRating: (UserRating: string) => set({ userRating: UserRating }),
+}));
+
+// movies
+type MoviesState = {
+  movies: Movie[];
+  setMovies: (movies: Movie[]) => void;
+};
+export const useMovieStore = create<MoviesState>((set) => ({
+  movies: [],
+  setMovies: (movies: Movie[]) => set({ movies: movies }),
+}));
+
+// selectedMovie
+type SelectedMovieState = {
+  selectedMovie: SelectedMovie | object;
+  setSelectedMovie: (selectedMovie: SelectedMovie | object) => void;
+};
+export const useSelectedMovieStore = create<SelectedMovieState>((set) => ({
+  selectedMovie: {},
+  setSelectedMovie: (selectedMovie: SelectedMovie | object) =>
+    set({ selectedMovie }),
 }));
 
 // watchedMovies
